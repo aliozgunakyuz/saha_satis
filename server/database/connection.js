e2dd78fbe37cb3521b45893bede7bf6ca2874a9f
aliozgunakyuz <ozgunakyuz@yahoo.com>
2023-07-20 15:02:43 +0300

    mongo atlas connection established

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,12 +1,14 @@
 import mongoose from 'mongoose';
 import {MongoMemoryServer} from "mongodb-memory-server";
+import ENV from '../config.js'
 
 async function connect(){
     const mongod = await MongoMemoryServer.create();
     const getUri = mongod.getUri();
 
     mongoose.set('strictQuery', true);
-    const db = await mongoose.connect(getUri);
+    //const db = await mongoose.connect(getUri);
+    const db = await mongoose.connect(ENV.ATLASURI);
     console.log("DB Connection Established");
     return db;
 }
