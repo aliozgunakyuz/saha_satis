c088859e4132a7491b9c601e6a532d0fd9026d86
aliozgunakyuz <ozgunakyuz@yahoo.com>
2023-08-17 12:14:37 +0300

    persistent login fixed

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,9 +1,16 @@
-import {create} from 'zustand';
+import { create } from 'zustand';
+import { persist, createJSONStorage, StateStorage } from 'zustand/middleware';
 
-export const useAuthStore = create((set)=>({
-    auth : {
+export const useAuthStore = create(persist((set, get) => ({
+    auth: {
         mail: '',
         active: false
     },
-    setMail:(mail) => set((state)=>({auth:{ ...state.auth, mail:mail }}))
-}))
+    setMail: (mail) => set((state) => ({ auth: { ...state.auth, mail: mail } })),
+
+}),
+    {
+        name: "saha-satis",
+        persist: createJSONStorage(() => localStorage),
+    }
+))
