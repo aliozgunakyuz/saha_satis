d71c705d5e2749f7a2f65161420cebccf51ee150
Ali Özgün Akyüz <74357615+aliozgunakyuz@users.noreply.github.com>
2023-08-29 11:54:31 +0300

    Delete config.js

--- server/config.js
+++
@@ -1,6 +0,0 @@
-export default {
-    JWT_SECRET: "aReMZjzrW4nR9c1OEnPWbTmglkdriRqoD/xpLgeajfs=",
-    EMAIL: "anya.kohler49@ethereal.email", //testing mail
-    PWD:  "aFckAqHAvVbfeAbY6V",
-    ATLASURI: "mongodb+srv://DB_admin:Rm79enzzN6MoU7ze@saleappdbmongo.2bu97cr.mongodb.net/?retryWrites=true&w=majority"
-}
